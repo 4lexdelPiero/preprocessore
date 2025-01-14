03286d62ed7281d41f2ebfd58e85c9bbc1675d90
4lexdelPiero <alessandro.marino@davincimilazzo.edu.it>
2025-01-14 13:02:44 +0100

    Add files via upload
    es2 tpsi

---
+++ es2_tpsi.c
@@ -0,0 +1,7 @@
+
+int main(void){
+	printf("abs(-1) = %d\n"  , ABSOLUTE(-1));
+	printf("abs(3-4) = %d\n" , ABSOLUTE(3-4));
+	printf("abs(7) = %d\n"   , ABSOLUTE(7));
+	printf("abs(-8-3) = %d\n", ABSOLUTE(-8-3));
+	return 0;
